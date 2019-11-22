932afe9db49f5ed9be0ef798355fe161b55c3a30
andela-awofadeju <olayemi.awofadeju@andela.com>
2019-11-22 13:30:18 +0100

    implement all feedback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::API
-  def not_found
-    render json: { error: 'not found' }
-  end
-
   def authorize_request
     header = request.headers['Authorization']
     token = header.split('').last if header
