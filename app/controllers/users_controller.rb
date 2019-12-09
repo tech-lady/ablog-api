45b5d69609c8c898505533ea74247513752877f0
andela-awofadeju <olayemi.awofadeju@andela.com>
2019-12-09 12:30:35 +0100

    crud for articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
   private
 
   def find_user
-    @user = User.find(params[:id)
+    @user = User.find(params[:id])
     rescue ActiveRecord::RecordNotFound
       render json: { errors: 'User not found' }, status: :not_found
   end
