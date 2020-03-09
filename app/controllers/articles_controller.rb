27400b442809f8a78b6b5b56e242ef4f4e863f27
andela-awofadeju <olayemi.awofadeju@andela.com>
2020-03-09 12:19:33 +0100

    implement feedback here

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show update destroy]
-  # before_action :authorize_request
+
   def index
     @articles = current_user.articles
       json_response(@articles)
